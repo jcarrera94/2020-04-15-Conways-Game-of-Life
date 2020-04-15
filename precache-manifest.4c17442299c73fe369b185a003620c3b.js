c9414045980a520a72e4be0380333935022b4306
Juan Carrera <juankarrera@gmail.com>
2020-04-15 15:02:08 -0400

    Updates

---
+++ precache-manifest.4c17442299c73fe369b185a003620c3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b709b14c6fbe13bbcb3cf7729b6043fe",
+    "url": "/2020-04-15-Conways-Game-of-Life/index.html"
+  },
+  {
+    "revision": "5fc28ba7b53ee4fc85c1",
+    "url": "/2020-04-15-Conways-Game-of-Life/static/css/main.fd0a79bf.chunk.css"
+  },
+  {
+    "revision": "914f4841044e45d7b7f0",
+    "url": "/2020-04-15-Conways-Game-of-Life/static/js/2.27bbeaec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020-04-15-Conways-Game-of-Life/static/js/2.27bbeaec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc28ba7b53ee4fc85c1",
+    "url": "/2020-04-15-Conways-Game-of-Life/static/js/main.2ba6c885.chunk.js"
+  },
+  {
+    "revision": "42e5e4d45f03fc18b850",
+    "url": "/2020-04-15-Conways-Game-of-Life/static/js/runtime-main.0e174200.js"
+  }
+]);
